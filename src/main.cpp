e5e05822f0769730d0951683d59cbe24916f4e55
dominik <01197296@pw.edu.pl>
2026-03-27 12:46:52 +0100

    deps: link SFML

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Graphics.hpp>
+#include <optional>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "HeroesAI");
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+
+    return 0;
+}
